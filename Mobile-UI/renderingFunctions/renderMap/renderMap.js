2e0a8a9f341822b7b0d8cb644136146891d82f12
Carlitzo <adrianscholl95@hotmail.com>
2025-04-09 12:55:06 +0200

    QWE

--- Mobile-UI/renderingFunctions/renderMap/renderMap.js
+++ Mobile-UI/renderingFunctions/renderMap/renderMap.js
@@ -1,3 +1,19 @@
+import { openAppFunction } from "../../appFunctions/openAppFunction.js";
+import { closeAppFunction } from "../../appFunctions/closeAppFunction.js";
+
 export function renderMap(mapIcon) {
-    
+    const mapImg = document.createElement("img");
+    const mapReturnButton = document.createElement("button");
+    mapImg.src = "./images/karta.jpg";
+    mapImg.id = "mapImg";
+    mapReturnButton.id = "mapReturnButton";
+    mapReturnButton.textContent = "X";
+
+    mapImg.appendChild(mapReturnButton);
+
+    openAppFunction(mapIcon, mapImg);
+
+    mapReturnButton.addEventListener("click", () => {
+        closeAppFunction(mapImg, mapIcon);
+    })
 }

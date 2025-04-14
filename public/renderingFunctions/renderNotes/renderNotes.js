c892930a4181f30888f6c6285b366a742abf837c
LucasFilipsson <143547032+LucasFilipsson@users.noreply.github.com>
2025-04-14 14:41:14 +0200

    fixed the css for notes

--- public/renderingFunctions/renderNotes/renderNotes.js
+++ public/renderingFunctions/renderNotes/renderNotes.js
@@ -5,13 +5,29 @@ import { closeAppFunction } from "../../appFunctions/closeAppFunction.js";
 export function renderNotes(notesIcon) {
 
     const notesDiv = document.createElement("div");
-    const notesReturnButton = document.createElement("button");
+    const notesDate = document.createElement("p");
+    const notesReturnText = document.createElement("p");
     const notesHeader = document.createElement("p");
+    const returnContainer = document.createElement("div");
+    const returnContainerLeft = document.createElement("div");
+    const returnImageArrow = document.createElement("img");
+    const shareImgage = document.createElement("img");
+    const moreIcon = document.createElement("img");
     notesDiv.id = "notesDiv";
-    notesReturnButton.id = "notesReturnButton";
-    notesReturnButton.textContent = "< Anteckningar";
+    notesDate.id ="notesDate";
+    notesDate.textContent = "13 April 2025 17:39";
+    notesReturnText.id = "notesReturnButton";
+    notesReturnText.textContent = "Anteckningar";
     notesHeader.id = "notesHeader";
     notesHeader.textContent = "Samlade saker";
+    returnContainer.id = "returnContainer";
+    returnContainerLeft.id = "returnContainerLeft";
+    returnImageArrow.id = "returnImageArrow";
+    returnImageArrow.src = "./icons/arrow-notes.png";
+    shareImgage.id = "shareImg";
+    shareImgage.src = "./icons/shareIcon.png";
+    moreIcon.id = "moreIcon";
+    moreIcon.src = "./icons/moreIcon.png";
 
     const tasks = ["Hämta kaffefilter från Orkanen", "Prata med Göran om kaffemått", "Gå till varvsparken", "Gå till agilitybanan", "Ta emot kaffebönor av Kerstin"];
     const tasksContainer = document.createElement("div");
@@ -26,13 +42,20 @@ export function renderNotes(notesIcon) {
         `;
     }
 
-    notesDiv.appendChild(notesReturnButton);
+    notesDiv.appendChild(notesDate);
     notesDiv.appendChild(notesHeader);
     notesDiv.appendChild(tasksContainer);
+    notesDiv.appendChild(returnContainer);
+    returnContainerLeft.appendChild(returnImageArrow);
+    returnContainerLeft.appendChild(notesReturnText);
+    returnContainer.appendChild(returnContainerLeft);
+    returnContainer.appendChild(shareImgage);
+    returnContainer.appendChild(moreIcon);
+
 
     openAppFunction(notesIcon, notesDiv);
 
-    notesReturnButton.addEventListener("click", () => {
+    returnContainerLeft.addEventListener("click", () => {
         closeAppFunction(notesDiv, notesIcon);
     })
 }

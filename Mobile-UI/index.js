7bb746302b582fbf55ce90f31eecd44abe2ae99e
Carlitzo <adrianscholl95@hotmail.com>
2025-04-14 09:51:21 +0200

    Fixed server.js
    Finally fixed server.js and map/file-system

--- Mobile-UI/index.js
+++
@@ -1,5 +0,0 @@
-import { renderHomepage } from "./renderingFunctions/renderhomePage/renderHomepage.js";
-import { setAppEvents } from "./setAppEvents/setAppEvents.js";
-
-renderHomepage();
-setAppEvents();
---
+++ public/index.js
@@ -0,0 +1,5 @@
+import { renderHomepage } from "../renderingFunctions/renderhomePage/renderHomepage.js";
+import { setAppEvents } from "../setAppEvents/setAppEvents.js";
+
+renderHomepage();
+setAppEvents();

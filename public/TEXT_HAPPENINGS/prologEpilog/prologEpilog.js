c20982c39dc08c20eb82c8d328880f76c386245a
Carlitzo <adrianscholl95@hotmail.com>
2025-05-06 13:24:50 +0200

    Changed spellingerror

--- public/TEXT_HAPPENINGS/prologEpilog/prologEpilog.js
+++ public/TEXT_HAPPENINGS/prologEpilog/prologEpilog.js
@@ -30,7 +30,7 @@ export function showProlog(onComplete) {
   
       const prologText = document.createElement("div");
       prologText.id = "prologText";
-      prologText.innerText = `Olle Ollessons mamma har massiva skulder på Klarna. Som den översittande shopaholic Karin är pressar hon därför sin enda son Olle att jobba heltid på Espresso House – samtidigt som han pluggar heltid. Detta har lett till att Olle, som även lider av social ångest och återkommande utbrändhet, nu ligger efter i skolan. När han en vanlig tisdag eftermiddag kommer till jobbet upptäcker han till sin förundran att kafét är nedstängt. En konstig känsla sprider sig i Olle men han skakar av sig den. Han bestämmer sig istället för att åka in till skolan för att ha det där kompletteringsamtalet med sin läraren Johannes. På skolan är allt som vanligt… nästan.`;
+      prologText.innerText = `Olle Ollessons mamma har massiva skulder på Klarna. Som den översittande shopaholic Karin är pressar hon därför sin enda son Olle att jobba heltid på Espresso House – samtidigt som han pluggar heltid. Detta har lett till att Olle, som även lider av social ångest och återkommande utbrändhet, nu ligger efter i skolan. När han en vanlig tisdag eftermiddag kommer till jobbet upptäcker han till sin förundran att kafét är nedstängt. En konstig känsla sprider sig i Olle men han skakar av sig den. Han bestämmer sig istället för att åka in till skolan för att ha det där kompletteringsamtalet med sin lärare Johannes. På skolan är allt som vanligt… nästan.`;
   
       prologContainer.appendChild(prologText);
       document.body.appendChild(prologContainer);

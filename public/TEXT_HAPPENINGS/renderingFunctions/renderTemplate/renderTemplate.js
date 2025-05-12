cb9f8cf9b212726d3b7d3f52cf17b9cafe8cd40e
Carlitzo <adrianscholl95@hotmail.com>
2025-05-12 15:14:39 +0200

    Fixed video size and forward and back arrows in text events

--- public/TEXT_HAPPENINGS/renderingFunctions/renderTemplate/renderTemplate.js
+++ public/TEXT_HAPPENINGS/renderingFunctions/renderTemplate/renderTemplate.js
@@ -4,23 +4,30 @@ export function renderTemplate() {
     const personSpeakingImg = document.createElement("img");
     const textBubble = document.createElement("div");
     const textBubbleText = document.createElement("p");
-    const arrowContainer = document.createElement("div");
+    const arrowRightContainer = document.createElement("div");
+    const arrowLeftContainer = document.createElement("div");
     const arrowImg = document.createElement("img");
+    const arrowLeftImg = document.createElement("img");
 
     wrapper.id = "wrapper";
     personSpeakingWrapper.id = "personSpeakingWrapper";
     personSpeakingImg.id = "personSpeakingImg";
     textBubble.id = "textBubble";
     textBubbleText.id = "textBubbleText";
-    arrowContainer.id = "arrowContainer";
+    arrowRightContainer.id = "arrowRightContainer";
+    arrowLeftContainer.id = "arrowLeftContainer";
     arrowImg.id = "arrowImg";
     arrowImg.src = "./icons/arrow.svg";
+    arrowLeftImg.id = "arrowLeftImg";
+    arrowLeftImg.src = "./icons/arrow.svg";
 
     document.body.appendChild(wrapper); 
     wrapper.appendChild(textBubble);
     wrapper.appendChild(personSpeakingWrapper);
-    wrapper.appendChild(arrowContainer);
+    wrapper.appendChild(arrowRightContainer);
+    wrapper.appendChild(arrowLeftContainer);
     personSpeakingWrapper.appendChild(personSpeakingImg);
     textBubble.appendChild(textBubbleText);
-    arrowContainer.appendChild(arrowImg);
+    arrowRightContainer.appendChild(arrowImg);
+    arrowLeftContainer.appendChild(arrowLeftImg);
 }
